f17d0f5853e6a91d04c15d399128106612fd3925
KevinSilva <kevinsilvadb503@gmail.com>
2024-07-24 10:00:20 -0600

    Practica de spring boot

---
+++ src/main/java/com/example/kdsb20240724/Kdsb20240724Application.java
@@ -0,0 +1,13 @@
+package com.example.kdsb20240724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdsb20240724Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kdsb20240724Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kdsb20240724/Kdsb20240724ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdsb20240724;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdsb20240724ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
